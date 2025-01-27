f7893107b1344597a8c1623b1963caead7e8096c
markeng25 <lopezcarlossaul011@gmail.com>
2025-01-27 00:14:51 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,35 @@
+// tono de luz opaca al iniciar la pagina
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.add('loading');
+});
+
+window.addEventListener('load', () => {
+    document.body.classList.remove('loading');
+    document.body.classList.add('loaded');
+});
+
+// audio de la musica de la pagina
+
+document.addEventListener('DOMContentLoaded', function() {
+    var audio = document.getElementById('mi-audio');
+
+    // Detectar un toque o clic para activar el audio
+    document.addEventListener('click', function() {
+        audio.muted = false;  // Desmutear el audio
+        audio.play().then(() => {
+            console.log("Audio desmuteado y reproduciéndose.");
+        }).catch((error) => {
+            console.log("Error al intentar reproducir el audio:", error);
+        });
+    }, { once: true }); // Se ejecuta solo una vez
+});
+
+  // JavaScript para el menú hamburguesa
+  const menuToggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('menu');
+
+  menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('show');
+  });
+
